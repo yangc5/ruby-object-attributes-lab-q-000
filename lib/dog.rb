bbfe81734e3ab104c3435e74867af9c62540b5af
Chen Yang <yangc5@mail.uc.edu>
2015-12-30 19:38:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    return @name
+  end
+  
+  def breed=(breed)
+    @breed=breed
+  end
+  
+  def breed
+    return @breed
+  end
+end
+
